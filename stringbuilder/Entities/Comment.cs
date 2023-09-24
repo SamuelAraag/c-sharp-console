90fc640a4623a55062b75994fa4e731a95182b46
Samuel Araag <samuelaraag@Samuels-MacBook-Air.local>
2023-09-24 14:53:27 -0300

    includes project console of stringbuilder

---
+++ stringbuilder/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Stringbuilder.Entities
+{
+	public class Comment
+	{
+		public Comment()
+		{
+		}
+
+		public string Text { get; set; }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
+
---
+++ stringbuilder/Program.cs
@@ -0,0 +1,19 @@
+﻿using Stringbuilder.Entities;
+using System.Globalization;
+
+Comment c1 = new Comment("Have a nice trip");
+Comment c2 = new Comment("Thats awasome");
+DateTime.TryParseExact("21/06/2018", "dd/MM/yyyy", new CultureInfo("pt-BR"), DateTimeStyles.None, out DateTime date);
+
+Post post = new Post(
+    date,
+    "Traveling to the New Zealand",
+    "I'm goin to this city",
+    12);
+
+post.AddComment(c1);
+post.AddComment(c2);
+
+Console.WriteLine(post);
+
+Console.ReadLine();
